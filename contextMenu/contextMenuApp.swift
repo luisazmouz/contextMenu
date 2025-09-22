a7180645a8e19dc47bbe51173474721c33901f3d
Seek-Connections <luisazmouz@gmail.com>
2025-09-22 18:12:04 -0400

    Initial Commit

---
+++ contextMenu/contextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  contextMenuApp.swift
+//  contextMenu
+//
+//  Created by Luis E. Azmouz on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct contextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
